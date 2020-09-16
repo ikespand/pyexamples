aec48b1b5c4067a85af05e8f54aa5031ad6d2a94
Sandeep Pandey <ikespand@Sandeeps-MBP.fritz.box>
2020-09-16 21:47:28 +0200

    First version of API

--- RestAPI/main_api.py
+++ RestAPI/main_api.py
@@ -1,50 +1,68 @@
 """
-Copyright (C) 2020 Kratrim Labs - All Rights Reserved.
+ikespand@GitHub
+
+Implemention of a REST API for our function which itself fetch data from the
+OSM and do some calculation based on Flask.
 """
 import flask
-from flask import request, jsonify, render_template
-
-import pandas as pd
+from flask import request, render_template
 from request_data_osm import FindToilets
-import requests
 
+# http://127.0.0.1:5000/api/v1/toilets?src=12.973132,77.601479&radius=500
+# results = near_toilets("12.973132,77.601479",None)
+# %%
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-#http://127.0.0.1:5000/api/v1/routes?src=12.973132,77.601479&tgt=12.977664,77.638843
-#results = find_route("12.973132,77.601479","12.977664,77.638843")
+
+def near_toilets(coords_string, radius):
+    """Returns nearby toiletse"""
+    lat = float(coords_string.split(',')[0])
+    lon = float(coords_string.split(',')[1])
+    results = FindToilets(lat=lat, lon=lon,
+                          radius=radius).provide_dataframe()
+    return results
+
 
 @app.route('/')
 def my_form():
+    """Home page of our api"""
     return render_template('my-form.html')
+
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    start = request.form['start']
-    end = request.form['end']
-    results = find_route(str(start), str(end))
-    return results.__geo_interface__
+    """Take the data from our form and provide the results."""
+    src = request.form['src']
+    if not len(src) > 0:
+        return "Error: No or bad source field!"
+    elif 'radius' in request.form['radius']:
+        radius = request.form['radius']
+        results = near_toilets(src, radius)
+        return results.__geo_interface__
+    else:
+        results = near_toilets(src, None)
+        return results.__geo_interface__
 
 
-@app.route('/api/v1/routes', methods=['GET'])
+@app.route('/api/v1/toilets', methods=['GET'])
 def api_id():
+    """Returns the data with web request."""
     if 'src' in request.args:
         src = request.args['src']
     else:
         return "Error: No src field provided."
 
-    if 'tgt' in request.args:
-        tgt = request.args['tgt']
+    if 'radius' in request.args:
+        radius = request.args['radius']
     else:
-        return "Error: No tgt field provided."
+        return "Error: No radius field provided."
 
-    # Create an dummy list for our results
-    results = find_route(src, tgt)
-    
-    #here_api = "5CXu_n0rUCdURLzwlrWd42CFNXvbMP0ceiPFyjTWIJQ"
-    #address= "https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey="+here_api+"&waypoint0=geo!"+src+"&waypoint1=geo!"+tgt+"&departure=now&mode=fastest;publicTransport&combineChange=true"
-    #response = requests.get(address)
+    results = near_toilets(src, radius)
 
-    return results.__geo_interface__#jsonify(results)
+    return results.__geo_interface__
 
-app.run()
+
+# %%
+if __name__ == "__main__":
+    app.run()
--- RestAPI/request_data_osm.py
+++ RestAPI/request_data_osm.py
@@ -1,28 +1,28 @@
 """
 ikespand@GitHub
 
-A sample program to fetch the data from OSM (via Overpass) for a given 
+A sample program to fetch the data from OSM (via Overpass) for a given
 coordinate as centre and a radius, to search for a toilet and its direct
 distance (which may not be the exact distance depending actual streets).
 Therefore, this example illustrates:
     1. How to use web api via request
     2. Postprocess the json data with pandas
-    3. Create our own API with postprocess data with`main_api.py` 
+    3. Create our own API with postprocess data with`main_api.py`
 """
 
 import geopandas as gpd
 import pandas as pd
 import math
 import requests
-import numpy as np 
-from shapely.geometry import Point, LineString
-from itertools import groupby
-from settings import *  # THIS HOLDS API KEYS
-from keplergl_cli.keplergl_cli import Visualize
+from shapely.geometry import Point
+# from settings import *  # THIS HOLDS API KEYS
+# from keplergl_cli.keplergl_cli import Visualize
 
-#%% INPUTS
+# %% Program
+
+
 class FindToilets():
-    def __init__(self,radius=None,lat=None,lon=None):
+    def __init__(self, radius=None, lat=None, lon=None):
         if radius is None:
             self.radius = 1000
             print("No radius is provide, choosing as 100 m")
@@ -38,7 +38,7 @@ class FindToilets():
             print("No coordinates? Choosing default '48.689357,9.000451'")
         else:
             self.lon = lon  # Should come from the request
-        #self.coord = self.lat, self.lon
+        # self.coord = self.lat, self.lon
         self.query = self.build_query()
         self.sever = 'http://overpass-api.de/api/interpreter'
 
@@ -52,54 +52,55 @@ class FindToilets():
             osm_id.append(data[i]['id'])
             lan_lon.append(Point(data[i]['lat'], data[i]['lon']))
             tags.append(data[i]['tags'])
-            distance_from_src.append(FindToilets.calculate_distance(self.lat, 
-                                                        self.lon,
-                                                        data[i]['lat'], 
-                                                        data[i]['lon']
-                                                        ))
-        df = pd.DataFrame({'osm_id':osm_id,
-                           'tags':tags,
-                           'distance_from_src':distance_from_src})
+            distance_from_src.append(FindToilets.calculate_distance(self.lat,
+                                                                    self.lon,
+                                                                    data[i]['lat'],
+                                                                    data[i]['lon']
+                                                                    ))
+        df = pd.DataFrame({'osm_id': osm_id,
+                           'tags': tags,
+                           'distance_from_src': distance_from_src})
         gdf = gpd.GeoDataFrame(df, geometry=lan_lon)
         return gdf
 
-        
     def build_query(self):
-        query_part_1 ='[out:json][timeout:25];(node["amenity"="toilets"]'
-        query_part_2 = '(around:'+str(self.radius)+','+str(self.lat)+','+ str(self.lon)+'); );'
-        query_part_3 = 'out body;>;out skel qt;'
+        query_part_1 = '[out:json][timeout:25];(node["amenity"="toilets"](around:'
+        query_part_2 = str(self.radius)+','+str(self.lat)+',' + str(self.lon)
+        query_part_3 = '); );out body;>;out skel qt;'
         return query_part_1+query_part_2+query_part_3
 
     def fetch_data_from_overpass(self):
         response = requests.get(self.sever, params={'data': self.query})
         if response.status_code == 200:
-            #print('Success!')
+            # print('Success!')
             return response.json()['elements']
         elif response.status_code == 404:
-            print('Error')  
+            print('Error')
         return [0]
-    
+
     @staticmethod
     def calculate_distance(lat1, lon1, lat2, lon2):
-        """ Using the haversine formula, which is good approximation even at
-        small distances unlike the Shperical Law of Cosines. This method has
-        ~0.3% error built in.
+        """ Using the haversine formula, it has ~0.3% error built in.
         """
-        
-        dLat = math.radians(float(lat2) - float(lat1))
-        dLon = math.radians(float(lon2) - float(lon1))
+
+        d_lat = math.radians(float(lat2) - float(lat1))
+        d_lon = math.radians(float(lon2) - float(lon1))
         lat1 = math.radians(float(lat1))
         lat2 = math.radians(float(lat2))
-    
-        a = math.sin(dLat/2) * math.sin(dLat/2) + \
-            math.cos(lat1) * math.cos(lat2) * math.sin(dLon/2) * \
-            math.sin(dLon/2)
-    
+
+        a = math.sin(d_lat/2) * math.sin(d_lat/2) + \
+            math.cos(lat1) * math.cos(lat2) * math.sin(d_lon/2) * \
+            math.sin(d_lon/2)
+
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    
+
         d = 6371 * c
-    
+
         return d
-    
+
+
 # %%
-find_wc = FindToilets().provide_dataframe()
+if __name__ == "__main__":
+    find_wc = FindToilets(radius=600,lat=12.973132,
+                          lon=77.601479).provide_dataframe()
+    print(find_wc.__geo_interface__)
