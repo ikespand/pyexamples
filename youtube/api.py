7d7932b82fc68326321d9e05d3ea0142d6eb06a4
ikespand <spandey.ike@gmail.com>
2021-11-21 13:36:12 +0530

    Restructured in OOP

--- youtube/api.py
+++ youtube/api.py
@@ -15,18 +15,30 @@ import os
 from pathlib import Path
 import pandas as pd
 
-# My YouTube API key is stored in environment variable, so getting them from 
-# there. Alternatively, one can paste directly here.
-api_key = os.environ["YOUTUBEAPIKEY"]
-
 # %%
 
 class YoutubeApi():
+    """Wrapper class for YouTube's official client from Google.
+    """
     def __init__(self, api_key):
         self.api_key = api_key
         self.youtube = build('youtube', 'v3', developerKey = self.api_key)
 
     def get_video_ids_for_channel(self, channel_id):
+        """
+        Provides all video ids for a given channel.
+
+        Parameters
+        ----------
+        channel_id : TYPE
+            Channel id, can be obtained from a variety of sources.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
         
         contentdata = self.youtube.channels().list(id = channel_id,part='contentDetails').execute()
     
@@ -141,142 +153,167 @@ class YoutubeApi():
         return df
 
 # %%
-
-def get_channelid_from_video_url(api_key, video_url):
+class CustomYouTubeApi():
+    """ A custom YouTube API using request purely. If YouTube decide to change
+    its API then this can be obsolete.
     """
-    Returns the channel id for a given video url.
-
-    Parameters
-    ----------
-    api_key : TYPE
-        DESCRIPTION.
-    video_url : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    video_id = video_url.split("v=")[1]
-    url = r"https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}".format(video_id, api_key)
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        return resp.json()["items"][0]["snippet"]["channelId"]
-    else:
-        print("Something went wrong! See message : ", resp.text)
+    
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.BASE_URL = r"https://www.googleapis.com/youtube/v3/"
+    
+    def get_channelid_from_video_url(self, video_url):
+        """
+        Returns the channel id for a given video url.
+    
+        Parameters
+        ----------
+        video_url : TYPE
+            DESCRIPTION.
+    
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+    
+        """
+        video_id = video_url.split("v=")[1]
+        url = self.BASE_URL + r"videos?part=snippet&id={}&key={}".format(video_id, self.api_key)
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            return resp.json()["items"][0]["snippet"]["channelId"]
+        else:
+            print("Something went wrong! See message : ", resp.text)
         
-def get_channelid_from_username(api_key, user_name):
-    """
-    Sometime doesn't work, therefore use: https://www.youtube.com/account_advanced
-    to find your user id and channel id.
-    
-    Returns the FIRST channel's ID for a given user if user has a YT channel.
-    TODO: Extend it to provide all channel's id.
-
-    Parameters
-    ----------
-    api_key : TYPE
-        DESCRIPTION.
-    user_name : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    url = r"https://www.googleapis.com/youtube/v3/channels?key={}&forUsername={}&part=id".format(api_key, user_name)
-    resp = requests.get(url)
-    if resp.json()["items"][0]["kind"] == "youtube#channel":
-        return resp.json()["items"][0]["id"]
-    else:
-        print("Not a YouTube channel")
+    def get_channelid_from_username(self, user_name):
+        """
+        Sometime doesn't work, therefore use: https://www.youtube.com/account_advanced
+        to find your user id and channel id. Otherwise, use `get_channelid_from_video_url()`
+        which will provide the channel id directly from a video URL.
         
+        Returns the FIRST channel's ID for a given user if user has a YT channel.
+        TODO: Extend it to provide all channel's id.
+    
+        Parameters
+        ----------
+        api_key : TYPE
+            DESCRIPTION.
+        user_name : TYPE
+            DESCRIPTION.
+    
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+    
+        """
+        url = self.BASE_URL + r"channels?key={}&forUsername={}&part=id".format( self.api_key , user_name)
+        resp = requests.get(url)
+        if resp.json()["items"][0]["kind"] == "youtube#channel":
+            return resp.json()["items"][0]["id"]
+        else:
+            print("Not a YouTube channel")
         
-def get_thumbnail(video_id, output_dir = os.getcwd()):
-    """
-    Download the thumbnail from a given video id.
-
-    Parameters
-    ----------
-    video_id : TYPE
-        DESCRIPTION.
-    output_dir : TYPE, optional
-        DESCRIPTION. The default is os.getcwd().
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    url = r"https://img.youtube.com/vi/{}/0.jpg".format(video_id)
-    response = requests.get(url)
-    Path(output_dir).mkdir(parents=True, exist_ok=True)    
-    if response.status_code == 200:
-        with open(os.path.join(output_dir, video_id+".jpg"), 'wb') as f:
-            f.write(response.content)
-    return os.path.join(output_dir, video_id+".jpg")
-
-
-def search_video_with_keywords(keywords, api_key, max_results = 2):
-    url = r"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults={}&q={}&type=video&key={}".format(max_results, keywords, api_key)
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        searched_videos = resp.json()["items"]
-        video_ids = []
-        publish_ts = []
-        title = []
-        thumbnail_def = []
-        thumbnail_mq = []        
-        thumbnail_hq = []
-        channel_title = []
-        for video in searched_videos:
-           video_ids.append(video["id"]["videoId"])
-           publish_ts.append(video["snippet"]["publishedAt"])
-           title.append(video["snippet"]["title"])
-           thumbnail_def.append(video["snippet"]["thumbnails"]["default"]["url"])
-           thumbnail_mq.append(video["snippet"]["thumbnails"]["medium"]["url"])
-           thumbnail_hq.append(video["snippet"]["thumbnails"]["high"]["url"])
-           channel_title.append(video["snippet"]["channelTitle"])         
+    @staticmethod
+    def get_thumbnail(video_id, output_dir = os.getcwd()):
+        """
+        Download the thumbnail from a given video id.
     
-        data={'video_id': video_ids,
-              'publish_ts':publish_ts,
-              'title':title,
-              'thumbnail_def':thumbnail_def,
-              'thumbnail_mq':thumbnail_mq,
-              'thumbnail_hq':thumbnail_hq,
-              'channel_title':channel_title}
-        return pd.DataFrame(data)    
+        Parameters
+        ----------
+        video_id : TYPE
+            DESCRIPTION.
+        output_dir : TYPE, optional
+            DESCRIPTION. The default is os.getcwd().
     
-    else:
-        print("Something went wrong with error message: {}".format(resp.text))
-        return None
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+    
+        """
+        url = r"https://img.youtube.com/vi/{}/0.jpg".format(video_id)
+        response = requests.get(url)
+        Path(output_dir).mkdir(parents=True, exist_ok=True)    
+        if response.status_code == 200:
+            with open(os.path.join(output_dir, video_id+".jpg"), 'wb') as f:
+                f.write(response.content)
+        return os.path.join(output_dir, video_id+".jpg")
 
-def get_video_stats_from_video_id(api_key, video_id):
-    url = r"https://www.googleapis.com/youtube/v3/videos?part=statistics&key={}&id={}".format(api_key, video_id)
-    #print(url)
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        vid_stat = resp.json()["items"][0]["statistics"]
-        vid_stat_df = pd.DataFrame(columns=list(vid_stat.keys()))
-        vid_stat_df.loc[0] = list(vid_stat.values())
-        vid_stat_df["video_id"] = video_id
-        return vid_stat_df
-    else:
-        print("Something went wrong with error message: {}".format(resp.text))
-        return None
-    
+
+    def search_video_with_keywords(self, keywords, max_results = 2):
+        """
+        Search functionality similar to YouTube website.
+
+        Parameters
+        ----------
+        keywords : TYPE
+            Keyword to search with. Tested with single keyword for now.
+        max_results : TYPE, optional
+            DESCRIPTION. The default is 2.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        url = self.BASE_URL + r"search?part=snippet&maxResults={}&q={}&type=video&key={}".format(max_results, keywords, self.api_key)
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            searched_videos = resp.json()["items"]
+            video_ids = []
+            publish_ts = []
+            title = []
+            thumbnail_def = []
+            thumbnail_mq = []        
+            thumbnail_hq = []
+            channel_title = []
+            for video in searched_videos:
+               video_ids.append(video["id"]["videoId"])
+               publish_ts.append(video["snippet"]["publishedAt"])
+               title.append(video["snippet"]["title"])
+               thumbnail_def.append(video["snippet"]["thumbnails"]["default"]["url"])
+               thumbnail_mq.append(video["snippet"]["thumbnails"]["medium"]["url"])
+               thumbnail_hq.append(video["snippet"]["thumbnails"]["high"]["url"])
+               channel_title.append(video["snippet"]["channelTitle"])         
+        
+            data={'video_id': video_ids,
+                  'publish_ts':publish_ts,
+                  'title':title,
+                  'thumbnail_def':thumbnail_def,
+                  'thumbnail_mq':thumbnail_mq,
+                  'thumbnail_hq':thumbnail_hq,
+                  'channel_title':channel_title}
+            return pd.DataFrame(data)    
+        
+        else:
+            print("Something went wrong with error message: {}".format(resp.text))
+            return None
+
+    def get_video_stats_from_video_id(self, video_id):
+        url = self.BASE_URL + r"videos?part=statistics&key={}&id={}".format(self.api_key, video_id)
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            vid_stat = resp.json()["items"][0]["statistics"]
+            vid_stat_df = pd.DataFrame(columns=list(vid_stat.keys()))
+            vid_stat_df.loc[0] = list(vid_stat.values())
+            vid_stat_df["video_id"] = video_id
+            return vid_stat_df
+        else:
+            print("Something went wrong with error message: {}".format(resp.text))
+            return None
+        
 #%%
 
-if __name__ == "__main__":    
+if __name__ == "__main__":   
+    # My YouTube API key is stored in environment variable, so getting them from 
+    # there. Alternatively, one can paste directly below.    
+    api_key = os.environ["YOUTUBEAPIKEY"]
     # 1. Get channel id from a video url 
-        # OR Get ur channel id from: https://www.youtube.com/account_advanced
-    
-    channel_id = get_channelid_from_video_url(api_key, r"https://www.youtube.com/watch?v=B8asvNNZuoA")
+    # OR Get ur channel id from: https://www.youtube.com/account_advanced
+    custom_api = CustomYouTubeApi(api_key)
+    channel_id = custom_api.get_channelid_from_video_url(r"https://www.youtube.com/watch?v=th5_9woFJmk")
     
     # 2. Get video id for the entire channel
     yt = YoutubeApi(api_key)
@@ -289,8 +326,8 @@ if __name__ == "__main__":
     # 3. Download the thumbnail and video stats
     video_stats = []
     for vid in video_ids:
-        __df = get_video_stats_from_video_id(api_key, vid)
-        __df["thumbnail_location"] = get_thumbnail(vid, output_dir = "./thumbnails")
+        __df = custom_api.get_video_stats_from_video_id(vid)
+        __df["thumbnail_location"] = custom_api.get_thumbnail(vid, output_dir = "./thumbnails")
         video_stats.append(__df)
             
     video_stats_df = pd.concat(video_stats)
