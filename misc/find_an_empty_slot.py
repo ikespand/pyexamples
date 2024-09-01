4368deb359975a53860e38cb98556aa74d089271
ikespand <spandey.ike@gmail.com>
2024-09-01 22:31:05 +0200

    Minor changes for WhatsApp message

--- misc/find_an_empty_slot.py
+++ misc/find_an_empty_slot.py
@@ -9,7 +9,7 @@ to find an empty slot via website and notfies you or your predefined contacts.
 
 This is for educational purpose! I personally am not aware about the legality of such tools.
 
-@author: PC
+@author: ikespand
 """
 from logbook import Logger, StreamHandler
 import sys
@@ -24,8 +24,18 @@ import re
 StreamHandler(sys.stdout).push_application()
 log = Logger('OktoBus')
 
-numbers=["+123456789"] # List of WhatsApp numbers for notification.
+numbers=["+1234567890"] # List of WhatsApp numbers for notification.
 
+url = r"https://www.oktoberfest-booking.com/de/reseller-angebote"
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+    # Add more user agents here
+]
+def get_random_user_agent():
+    return random.choice(user_agents)
 
 # Function to extract relevant information
 def extract_info(entry):
@@ -62,7 +72,7 @@ def extract_info(entry):
 def check_availability(query_date:str, query_tod:str):
     """Function to check availability for a date and ToD"""
     scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
-    url = r"https://www.oktoberfest-booking.com/de/reseller-angebote"
+    scraper.headers.update({'User-Agent': get_random_user_agent()})
     resp = scraper.get(url)
     #header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.188 Safari/537.36",
     #          "X-Requested-With": "XMLHttpRequest"}
@@ -87,31 +97,39 @@ def check_availability(query_date:str, query_tod:str):
         # print(df)
        
         for id, row in df.iterrows():
-            if query_date == row["date"]:
+            if query_date is None:
+                if query_tod == row["tod"]:
+                    log.info(f"SUCCESS! Found for query date {row['date']} for TOD {row['tod']}")
+                    return row
+            elif query_date == row["date"]:
                 #log.info(f"Found one on query date {row['date']} for time {row['tod']}")
                 if query_tod == row["tod"]:
                     log.info(f"SUCCESS! Found for query date {row['date']} for TOD {row['tod']}")
                     return row
         log.warning("Couldn't find any exact matching date/time. Current DATAFRAME is >>")
-        print(df)
+        print(df.to_string())
         return False
     else:
         log.error("REQUEST FAILED. See the raw response!")
         log.error(resp.text)
         return None
     
-def find_slot(query_date: str,query_tod:str, time_sleep = 300):
+def find_slot(query_date: str, query_tod:str):
     result = check_availability(query_date=query_date, query_tod=query_tod)
     if type(result)!=bool: # Dirty logic
         log.info("Notifying on whatsapp")
-        msg = f"[OktoBOT] I have found an empty slot on date *{result['date']}* for *{result['tod']}* for *{result['person']}*"
+        msg = f""" [OktoBOT] I have found an empty slot on in *{result['zelt']}* 
+        date *{result['date']}* for *{result['tod']}* -- *{result['time']}* 
+        for *{result['person']}*. It will cost *{result['totalcost']}*.
+        Book now by visiting {url}""
+        """
         send_whatsapp_message(numbers, msg)
         return True
     else:
-        time_sleep = random.randint(421,556)
+        time_sleep = random.randint(555,999)
         log.info(f"Retrying in {time_sleep} seconds")
         time.sleep(time_sleep)
-        find_slot(query_date, query_tod, time_sleep = time_sleep)
+        find_slot(query_date, query_tod)
 
 def send_whatsapp_message(numbers:list, msg: str):
     "Copied from Internet!"
@@ -129,8 +147,8 @@ def send_whatsapp_message(numbers:list, msg: str):
            
 # %%
 if __name__ == "__main__":
-    my_date = "01.10.2024"
+    my_date = None
     my_time = "Abend"
     print(f"STARTING TO QUERY FOR {my_date} and {my_time}")
-    success = find_slot(query_date=my_date, query_tod= my_time,time_sleep = 100)
+    success = find_slot(query_date=my_date, query_tod= my_time)
 
